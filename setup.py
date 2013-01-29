633b78e7607e1ed9c9a8f578cfae84b25ee601b0
shakefu <shakefu@gmail.com>
2013-01-29 12:57:51 -0800

    Update setup.py to improve long_description.

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import multiprocessing, logging
 from setuptools import setup
 
+
+def readme():
+    try:
+        return open('README', 'r').read()
+    except:
+        return 'Unofficial Python client for KISSmetrics'
+
+
 setup(
     name = 'pyKISSmetrics',
     packages = ['KISSmetrics'],
     version = '1.0.2',
     description = 'Unofficial Python client for KISSmetrics',
-    long_description = 'Unofficial Python client for KISSmetrics',
+    long_description = readme(),
     url = 'http://github.com/shakefu/pyKISSmetrics',
     author = 'kissmetrics',
     author_email = 'support@kissmetrics.com',
